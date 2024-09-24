24bafab27671a96574d53f8005e3abc8725ea5d9
jam3sval3ntin3 <jam3sval3ntin3@gmail.com>
2024-09-23 22:29:02 -0700

    second push

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// This function runs when the page loads
+window.onload = function() {
+    // Display a welcome alert
+    alert("Welcome to jam3sval3ntin3.com!");
+
+    // You can also manipulate the DOM, for example:
+    const body = document.body;
+    body.innerHTML += "<p>This is your JavaScript in action!</p>";
+};
