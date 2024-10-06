f5e68a1019a5932c17b8789b7c546741c061133a
jam3sval3ntin3 <jam3sval3ntin3@users.noreply.github.com>
2024-10-05 22:47:24 -0700

    Template updates

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// This function runs when the page loads
-window.onload = function() {
-    // Display a welcome alert
-    alert("Welcome to jam3sval3ntin3.com");
-
-    // You can also manipulate the DOM, for example:
-    const body = document.body;
-    body.innerHTML += "<p>d3ad insid3, still aliv3</p>";
-};
